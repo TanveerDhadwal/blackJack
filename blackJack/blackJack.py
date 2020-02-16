f693a09d03c5c62abd2ab8b33cdcae04f8e3d839
TanveerDhadwal <tanveerdhadwal@gmail.com>
2020-02-16 01:16:49 -0500

    algorithm completed

---
+++ blackJack/blackJack.py
@@ -0,0 +1,14 @@
+""" blackJack.py """
+import random
+class Card:
+    def __init__(self):
+        return "Null " +0
+    def makeCard(self):
+        suite=""
+        cardValue = 0
+        suite=suite()
+        cardValue=value()
+        return suite+" "+cardValue
+    def suite(self)
+        return ""
+
